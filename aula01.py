964175d48640a26161c8ed92ac0b9a6a3815c00c
Dehboraw <alvezdebora2007@gmail.com>
2026-02-20 08:04:23 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,10 @@
+
+print("Olá Mundo!")
+nome = "Débora"
+idade = 18
+altura = 1.60
+#Usar o print com variáveis: (uso de muitas vírgulas, pode acabar ficando confuso)
+#print("Olá, meu nome é",nome,",minha idade é",idade)
+
+#F-Strings
+print(f'Olá, meu nome é {nome}, minha idade é {idade}, e tenho {altura}m de altura')
